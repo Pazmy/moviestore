926f99d4896924eb2988211cd099a69dd175d3ae
Pazmy <fadhillah17azmy01@gmail.com>
2021-11-30 20:46:46 +0700

    add movie model

---
+++ backend/migrations/20211130125905-create-movie.js
@@ -0,0 +1,69 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Movies", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      director: {
+        type: Sequelize.STRING,
+      },
+      studio: {
+        type: Sequelize.STRING,
+      },
+      duration: {
+        type: Sequelize.STRING,
+      },
+      release: {
+        type: Sequelize.STRING,
+      },
+      genreId: {
+        type: Sequelize.INTEGER,
+      },
+      rate: {
+        type: Sequelize.STRING,
+      },
+      trailer: {
+        type: Sequelize.STRING,
+      },
+      views: {
+        type: Sequelize.STRING,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      poster: {
+        type: Sequelize.STRING,
+      },
+      overview: {
+        type: Sequelize.TEXT,
+      },
+      status: {
+        type: Sequelize.STRING,
+      },
+      episode: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Movies");
+  },
+};
---
+++ backend/models/movie.js
@@ -0,0 +1,41 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Movie extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Movie.hasMany(models.Actor);
+      Movie.belongsToMany(models.User, { through: models.Comment });
+      Movie.belongsToMany(models.Genre, { through: models.MovieGenre });
+      // Movie.belongsToMany(models.Order,{through:models.MovieOrder})
+    }
+  }
+  Movie.init(
+    {
+      title: DataTypes.STRING,
+      director: DataTypes.STRING,
+      studio: DataTypes.STRING,
+      duration: DataTypes.STRING,
+      release: DataTypes.STRING,
+      genreId: DataTypes.INTEGER,
+      rate: DataTypes.STRING,
+      trailer: DataTypes.STRING,
+      views: DataTypes.STRING,
+      price: DataTypes.INTEGER,
+      image: DataTypes.STRING,
+      poster: DataTypes.STRING,
+      overview: DataTypes.TEXT,
+      status: DataTypes.STRING,
+      episode: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Movie",
+    }
+  );
+  return Movie;
+};
