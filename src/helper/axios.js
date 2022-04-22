c383472ae3df0830c56be2bc480275b9db8560ef
Pazmy <fadhillah17azmy01@gmail.com>
2022-04-22 11:46:27 +0700

    update

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const SERVER_URL = "http://localhost:8080";
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const Instance = axios.create({
   baseURL: SERVER_URL,
